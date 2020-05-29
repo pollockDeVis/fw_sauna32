f637229bbd0637e333009d87fda8473ab0cbac4d
SadequrRahman <sadequr.rahman.rabby@gmail.com>
2020-05-30 00:39:13 +0200

    ble manager with gap service and gap api implemented

--- entropy/main/app.c
+++ entropy/main/app.c
@@ -10,6 +10,7 @@
 #include "esp_log.h"
 #include "nvs_flash.h"
 #include "heartBeat.h"
+#include "ble_manager.h"
 
 
 static const char *TAG = __FILE__;
@@ -25,6 +26,16 @@ void app_main()
         ret = nvs_flash_init();
     }
     ESP_ERROR_CHECK( ret );
+	
+	ble_manager_init();
+	ble_manager_setDeviceName("Rabby-Esp32");
+	esp_ble_adv_data_t *advData = ble_manager_getDefaultAdvData();
+	esp_ble_adv_params_t *advParams = ble_manager_getDefaultAdvertiseParam();
+	esp_ble_adv_data_t *scanResp = ble_manager_getDefaultAdvRespData();
+
+	ble_manager_setAdvParams(advParams);
+	ble_manager_setAdvRespData(scanResp);
+	ble_manager_setAdvData(advData);
 	heartBeat_init();
 
 }
