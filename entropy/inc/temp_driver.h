79ff4af0d973f871fba2b701bfa47082ce282400
palok94 <palok94@gmail.com>
2020-08-07 21:15:54 +0800

    tested on sipeed maixduino board's esp 32

--- entropy/inc/temp_driver.h
+++ entropy/inc/temp_driver.h
@@ -11,8 +11,8 @@
 #define MLX90614_DEFAULT_ADDRESS 		0x5A // default chip address(slave address) of MLX90614
 
 #define MLX90614_I2C_CLOCK_FREQUENCY	100000
-#define MLX90614_SDA_GPIO 				21 // sda for MLX90614
-#define MLX90614_SCL_GPIO 				22 // scl for MLX90614
+#define MLX90614_SDA_GPIO 				32 // sda for MLX90614
+#define MLX90614_SCL_GPIO 				33// scl for MLX90614
 #define MINIMUM_MEASURED_TEMP			10
 #define MAXIMUM_MEASURED_TEMP			99
 
